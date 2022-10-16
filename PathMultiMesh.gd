7a7f67bb4e6b9d663ffc2169c94b168ef7754aa1
Julius B <36518501+silver1bear@users.noreply.github.com>
2022-10-16 23:23:55 +0200

    fixed positioning of instances on curve

--- PathMultiMesh.gd
+++ PathMultiMesh.gd
@@ -15,19 +15,21 @@ var curve_length = 0
 var mm : MultiMesh
 
 func _ready():
-	if Engine.editor_hint:
+	if Engine.editor_hint: # only run in engine
 		var size = (m as Mesh).get_aabb().size.z
 		curve_length = curve.get_baked_length()
 		count = floor(curve_length/size) + 1
 		
+		# set up MultiMesh
 		mm = MultiMesh.new()
 		mm.transform_format = MultiMesh.TRANSFORM_3D
 		mm.color_format = MultiMesh.COLOR_8BIT
 		mm.mesh = m
 		mm.instance_count = count
 		
+		# follow along path and set MultiMesh Transforms accordingly
 		for i in range(0, count):
-			path_follow.unit_offset = i/count
+			path_follow.offset = size + i * size
 			var pf_t = path_follow.transform
 			var t = Transform(pf_t.basis, pf_t.origin)
 			mm.set_instance_transform(i, pf_t)
@@ -40,6 +42,7 @@ func _process(delta):
 			_ready()
 		curve_changed = false
 
+# detect change in curve to only recalculate if something was actually changed
 func _on_Path_curve_changed():
 	if Engine.editor_hint:
-		curve_changed = true
+		curve_changed = true 
